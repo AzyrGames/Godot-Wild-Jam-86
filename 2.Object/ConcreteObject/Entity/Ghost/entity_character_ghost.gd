4d765f2a93c8c0f3f00a81297fe91a4c134e2c61
superhighlevel <superhighlive@gmail.com>
2025-10-16 14:25:25 +0700

    Add signal and adjust player

--- 2.Object/ConcreteObject/Entity/Ghost/entity_character_ghost.gd
+++ 2.Object/ConcreteObject/Entity/Ghost/entity_character_ghost.gd
@@ -1,9 +1,9 @@
 extends EntityCharacter2D
 class_name EntityCharacterGhost2D
 
-@export var active : bool = false
+@export var active: bool = false
 
-@export var move_speed : float
+@export var move_speed: float
 @export var move_direction: Vector2
 
 var _last_move_direction: Vector2
@@ -18,8 +18,9 @@ func _physics_process(delta: float) -> void:
 
 
 func _input(event: InputEvent) -> void:
+	if !active: return
 	if event is InputEventKey:
-		move_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down",)
+		move_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down", )
 		if Input.is_action_just_pressed("move_jump"):
 			trigger_mask()
 	pass
@@ -30,8 +31,8 @@ func calculate_velocity() -> void:
 	pass
 
 
-
 func trigger_mask() -> void:
+	print("yes")
 	EventBus.mask_triggered.emit(global_position)
 	pass
 
