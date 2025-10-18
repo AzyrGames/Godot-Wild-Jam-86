bca434cc85c7fd7e6364e2426b858d0d59bb18a7
Setadokalo <darkenchanter1@gmail.com>
2025-10-17 21:11:22 -0500

    Add masking (#6)
    * make player switcher handle other sources of player switching
    
    * Add mask renderer and logic
    
    * make player model fit cleanly through 1x2 spaces

--- 3.System/PlayerSwitcher/player_switcher.gd
+++ 3.System/PlayerSwitcher/player_switcher.gd
@@ -11,25 +11,29 @@ var switch_cooldown: Timer
 
 func _ready() -> void:
 	create_switch_cooldown()
+	EventBus.character_switched.connect(_on_character_switch)
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("switch_character"):
 		switch_character()
 	pass
 
+func _on_character_switch(active_character: GameData.CharacterType) -> void:
+	if active_character == GameData.CharacterType.GHOST:
+		character_platformer.active = false
+		character_ghost.active = true
+		is_character_platformer = false
+	else:
+		character_platformer.active = true
+		character_ghost.active = false
+		is_character_platformer = true
 
 func switch_character() ->void:
 	if !switch_cooldown.is_stopped(): return
 	if is_character_platformer:
-		character_platformer.active = false
-		character_ghost.active = true
-
 		EventBus.character_switched.emit(GameData.CharacterType.GHOST)
 	else:
-		character_platformer.active = true
-		character_ghost.active = false
 		EventBus.character_switched.emit(GameData.CharacterType.PLATFORMER)
-	is_character_platformer = !is_character_platformer
 	switch_cooldown.start()
 	pass
 
